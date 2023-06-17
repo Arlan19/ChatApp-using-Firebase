26c8e93f37e11206267ca9f186ff1536d85858be
Arlan19 <arlanleonallacsta20@gmail.com>
2023-06-17 21:46:47 +0700

    Providing logic to LoginActivity by connecting to Firebase. and providing logic for Login Status

--- app/src/main/java/com/allacsta/chatapp/LoginActivity.java
+++ app/src/main/java/com/allacsta/chatapp/LoginActivity.java
@@ -1,14 +1,22 @@
 package com.allacsta.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
@@ -18,10 +26,16 @@ public class LoginActivity extends AppCompatActivity {
     private EditText userEmail, userPassword;
     private TextView needNewAccountLink, forgetPasswordLink;
 
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
         InitializeFields();
 
@@ -31,6 +45,44 @@ public class LoginActivity extends AppCompatActivity {
                 SendUserToRegisterActivity();
             }
         });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AllowUserToLogin();
+            }
+        });
+    }
+
+    private void AllowUserToLogin() {
+        String email = userEmail.getText().toString();
+        String password = userPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter Email..", Toast.LENGTH_SHORT).show();
+        }if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter Password..", Toast.LENGTH_SHORT).show();
+        }else {
+            loadingBar.setTitle("Sign In");
+            loadingBar.setMessage("Please wait...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        SendUserToMainActivity();
+                        Toast.makeText(LoginActivity.this, "Logged in Successful...", Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }else {
+                        String message = task.getException().toString();
+                        Toast.makeText(LoginActivity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                }
+            });
+        }
     }
 
     private void InitializeFields() {
@@ -40,7 +92,7 @@ public class LoginActivity extends AppCompatActivity {
         userPassword = (EditText) findViewById(R.id.login_password);
         needNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
         forgetPasswordLink = (TextView) findViewById(R.id.forget_password_link);
-
+        loadingBar = new ProgressDialog(this);
     }
 
     @Override
