5839f57c413e86425cae052d7e8fe79d3c4cf6d8
Arlan19 <arlanleonallacsta20@gmail.com>
2023-06-19 17:16:52 +0700

    Adding GroupChatActivity

--- app/src/main/java/com/allacsta/chatapp/GroupChatFragment.java
+++ app/src/main/java/com/allacsta/chatapp/GroupChatFragment.java
@@ -2,11 +2,25 @@ package com.allacsta.chatapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +29,12 @@ import android.view.ViewGroup;
  */
 public class GroupChatFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View groupChatFragmentView;
+    private ListView list_view;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_groups = new ArrayList<>();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private DatabaseReference groupRef;
 
     public GroupChatFragment() {
         // Required empty public constructor
@@ -40,8 +52,6 @@ public class GroupChatFragment extends Fragment {
     public static GroupChatFragment newInstance(String param1, String param2) {
         GroupChatFragment fragment = new GroupChatFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,15 +60,51 @@ public class GroupChatFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group_chat, container, false);
+
+        groupChatFragmentView = inflater.inflate(R.layout.fragment_group_chat, container, false);
+
+        groupRef = FirebaseDatabase.getInstance().getReference().child("Groups");
+
+        InitializeFields();
+
+        RetrieveAndDisplayGroups();
+
+        return groupChatFragmentView;
+    }
+
+    private void RetrieveAndDisplayGroups() {
+        groupRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Set<String> set = new HashSet<>();
+
+                Iterator iterator = snapshot.getChildren().iterator();
+
+                while (iterator.hasNext()){
+                    set.add(((DataSnapshot)iterator.next()).getKey());
+                }
+
+                list_of_groups.clear();
+                list_of_groups.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void InitializeFields() {
+        list_view = (ListView) groupChatFragmentView.findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, list_of_groups);
+        list_view.setAdapter(arrayAdapter);
     }
 }
