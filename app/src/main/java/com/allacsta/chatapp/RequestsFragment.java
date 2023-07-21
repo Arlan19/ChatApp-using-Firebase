b4f6c909826baf8e505e75e51569160a4b604ad2
Arlan19 <arlanleonallacsta20@gmail.com>
2023-07-22 02:56:38 +0700

    Display user Chat Request in RequestFragment

--- app/src/main/java/com/allacsta/chatapp/RequestsFragment.java
+++ app/src/main/java/com/allacsta/chatapp/RequestsFragment.java
@@ -2,6 +2,7 @@ package com.allacsta.chatapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,12 +10,30 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class RequestsFragment extends Fragment {
 
     private View requestsFragmentView;
     private RecyclerView myRequestsList;
+
+    private DatabaseReference chatRequestRef, usersRef;
+    private FirebaseAuth mAuth;
+    private String currentUserID;
 
     public RequestsFragment() {
         // Required empty public constructor
@@ -27,11 +46,107 @@ public class RequestsFragment extends Fragment {
         // Inflate the layout for this fragment
         requestsFragmentView = inflater.inflate(R.layout.fragment_requests, container, false);
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+
+        chatRequestRef = FirebaseDatabase.getInstance().getReference().child("Chat Requests");
+        usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
 
         myRequestsList = (RecyclerView) requestsFragmentView.findViewById(R.id.chat_request_list);
         myRequestsList.setLayoutManager(new LinearLayoutManager(getContext()));
 
 
         return requestsFragmentView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options = new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(chatRequestRef.child(currentUserID), Contacts.class).build();
+
+        FirebaseRecyclerAdapter<Contacts, RequestsViewHolder> adapter = new FirebaseRecyclerAdapter<Contacts, RequestsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull RequestsViewHolder holder, int position, @NonNull Contacts model) {
+                holder.itemView.findViewById(R.id.request_accept_btn).setVisibility(View.VISIBLE);
+                holder.itemView.findViewById(R.id.request_cancel_btn).setVisibility(View.VISIBLE);
+
+                final String list_user_id = getRef(position).getKey();
+
+                DatabaseReference getTypeRef = getRef(position).child("request_type").getRef();
+
+                getTypeRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()){
+                            String type = snapshot.getValue().toString();
+
+                            if (type.equals("received")){
+                                usersRef.child(list_user_id).addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        if (snapshot.hasChild("image")){
+                                            final String requestUserName = snapshot.child("name").getValue().toString();
+                                            final String requestUserStatus = snapshot.child("status").getValue().toString();
+                                            final String requestProfileImage = snapshot.child("image").getValue().toString();
+
+                                            holder.userName.setText(requestUserName);
+                                            holder.userStatus.setText(requestUserStatus);
+                                            Picasso.get().load(requestProfileImage).placeholder(R.drawable.profile_image).into(holder.profileImage);
+                                        }else {
+                                            final String requestUserName = snapshot.child("name").getValue().toString();
+                                            final String requestUserStatus = snapshot.child("status").getValue().toString();
+
+                                            holder.userName.setText(requestUserName);
+                                            holder.userStatus.setText(requestUserStatus);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public RequestsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_diplay_layout, parent, false);
+                RequestsViewHolder holder = new RequestsViewHolder(view);
+                return holder;
+            }
+        };
+
+        myRequestsList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class RequestsViewHolder extends  RecyclerView.ViewHolder{
+
+        TextView userName, userStatus;
+        CircleImageView profileImage;
+        Button acceptButton, cancelButton;
+
+        public RequestsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.user_profile_name);
+            userStatus = itemView.findViewById(R.id.user_status);
+            profileImage = itemView.findViewById(R.id.users_profile_image);
+            acceptButton = itemView.findViewById(R.id.request_accept_btn);
+            cancelButton = itemView.findViewById(R.id.request_cancel_btn);
+        }
     }
 }
